7cbd78d135a48815a8cfd47fcde5ab631a2222ea
Pino <alessandro.pini.filippini@gmail.com>
2021-10-15 16:52:28 +0200

    Horrible period validation Schema

---
+++ src/validations/period.validation.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+/* Accept either :
+    - Date in ISO format
+    - number of weekday */
+const periodSchema = Joi.alternatives().conditional(Joi.object({ from : Joi.date().iso()}).unknown(), {
+    then : Joi.object({
+        from: Joi.date().iso().required(),
+        to: Joi.date().iso().min(Joi.ref('from')) // min is used as >=
+    }),
+    otherwise : Joi.object({
+        from: Joi.number().min(0).max(6).integer().required(),
+        to: Joi.number().min(Joi.ref('from')).max(6).integer() // min is used as >=
+    })
+});
+
+
+module.exports = { periodSchema }
