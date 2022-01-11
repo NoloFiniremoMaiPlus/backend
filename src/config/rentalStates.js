7914a66007809fc4463d798157947560214ff704
Yureehh <fabbri.juri@gmail.com>
2022-01-11 19:03:47 +0100

    Nuovi Stati Noleggio

--- src/config/rentalStates.js
+++ src/config/rentalStates.js
@@ -1,5 +1,5 @@
 // Finished = endend time of rental
 // Completed = Rental close, payed eventual surcharges
-const rentalStates = ["Booked", "Accepted", "Ongoing", "Finished", "Completed"];
+const rentalStates = ["Booked", "Accepted", "Ongoing", "Expired", "Returned"];
   
 module.exports = rentalStates;
