506859fdf185659211b65f5be17af88b18278efd
Pino <alessandro.pini.filippini@gmail.com>
2022-01-12 11:49:10 +0100

    updated comments

--- src/config/rentalStates.js
+++ src/config/rentalStates.js
@@ -1,5 +1,10 @@
-// Finished = endend time of rental
-// Completed = Rental close, payed eventual surcharges
+/**
+ * Booked = user created rental
+ * Accepted = backoffice acceptd the rental
+ * Ongoing = user picked up the item rental
+ * Expired = user is late to return the item
+ * Returned = item returned and rent payed
+ */
 const rentalStates = ["Booked", "Accepted", "Ongoing", "Expired", "Returned"];
   
 module.exports = rentalStates;
